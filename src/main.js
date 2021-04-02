9dfc433c84e4c05f4dd69dfa4709d96efcef3b74
blizzardKv <makemewanna@yandex.ru>
2021-04-02 20:05:42 +0300

    константы вынесены после импортов, переименована функция, обертки для циклов #4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {createSiteMenuTemplate} from './view/menu';
+import {createMenu} from './view/menu';
 import {createFilmCard} from './view/filmCard';
 import {createUserRank} from './view/userRank';
 import {createFilmListContainer} from './view/filmListContainer';
@@ -7,47 +7,48 @@ import {createExtraFilmsWrapper} from './view/extraFilmsWrapper';
 import {createFooterStatistics} from './view/footerStatistics';
 import {createPopup} from './view/popup';
 
+const DEFAULT_CARDS_COUNT = 5;
+const EXTRA_CARDS_COUNT = 2;
+
 const renderComponent = (container, markup, insertPlace = 'beforeend') => {
   container.insertAdjacentHTML(insertPlace, markup);
-};
-
-// четыре операнда, наверно, слишком много для функции.
-const renderComponentCyclically = (iterationNumber, container, markup, insertPlace = 'beforeend') => {
-  for (let i = 0; i < iterationNumber; i++) {
-    renderComponent(container, markup, insertPlace);
-  }
 };
 
 const mainWrapper = document.querySelector('.main');
 const header = document.querySelector('.header');
 const footerStatistics = document.querySelector('.footer__statistics');
 
-const DEFAULT_CARDS_COUNT = 5;
-const EXTRA_CARDS_COUNT = 2;
-
 renderComponent(header, createUserRank());
-renderComponent(mainWrapper, createSiteMenuTemplate());
+renderComponent(mainWrapper, createMenu());
 renderComponent(mainWrapper, createFilmListContainer());
 
 const filmsSection = mainWrapper.querySelector('.films');
 const filmListWrapper = filmsSection.querySelector('.films-list__container');
 
-renderComponentCyclically(DEFAULT_CARDS_COUNT, filmListWrapper, createFilmCard());
+const renderFilmCardMultipleTimes = () => {
+  for (let i = 0; i < DEFAULT_CARDS_COUNT; i++) {
+    renderComponent(filmListWrapper, createFilmCard());
+  }
+};
+
+renderFilmCardMultipleTimes();
 
 renderComponent(mainWrapper, createShowMoreButton());
 renderComponent(filmsSection, createExtraFilmsWrapper('Top rated'));
 renderComponent(filmsSection, createExtraFilmsWrapper('Most commented'));
 
-// зависимый выбор селекторов, т.к. появляется возможность выбрать только после рендера компонентов с разметкой createExtraFilmsWrapper.
-// можно рендерить сразу и обертки, и карточку - добавить опциональный аргумент для createFilmCard().
 const extraWrappers = mainWrapper.querySelectorAll('.films-list--extra');
 
 extraWrappers.forEach((wrapper) => {
   const filmWrapper = wrapper.querySelector('.films-list__container');
-  renderComponentCyclically(EXTRA_CARDS_COUNT, filmWrapper, createFilmCard());
+  const renderFilmCardMultipleTimes = () => {
+    for (let i = 0; i < EXTRA_CARDS_COUNT; i++) {
+      renderComponent(filmWrapper, createFilmCard());
+    }
+  };
+  renderFilmCardMultipleTimes();
 });
 
 renderComponent(footerStatistics, createFooterStatistics());
 
-// должен рендерится динамически.
 renderComponent(mainWrapper, createPopup());
